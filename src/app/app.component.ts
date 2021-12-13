c5d4aed0171a00cc168b37bdb8d81711798cb12c
Selim GOMRI <selimgomri@gmail.comssh git github.comssh git@github.comcdssh git@github.comss-keygen -t rsagit config --global user.email selimgomri@gmail.com>
2021-12-13 15:09:10 +0100

    title renamed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Bienvenue sur le site de Selim !';
+  title = 'Bienvenue sur le site de Selim ! 🤖';
 }
